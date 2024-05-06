c778036357bf0ab1a83ee5eea7baf9cafbd7c22d
MarvsTech <115031416+Crispkreme@users.noreply.github.com>
2024-05-06 11:07:15 -0800

    updating the upcoming events

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,19 @@
-export { Header } from "./header";
+import DealsChart from './home/deals-chart';
+import UpcomingEvents from './home/upcoming-events';
+import UpcomingEventsSkeleton from './skeleton/upcoming-events';
+import ProjectCardSkeleton from './skeleton/project-card';
+import LatestActivitiesSkeleton from './skeleton/latest-activities';
+import KanbanColumnSkeleton from './skeleton/kanban';
+import AccordionHeaderSkeleton from './skeleton/accordion-header';
+
+export { Header } from './layout/header';
+
+export { 
+    AccordionHeaderSkeleton, 
+    UpcomingEvents, 
+    DealsChart, 
+    UpcomingEventsSkeleton, 
+    ProjectCardSkeleton, 
+    LatestActivitiesSkeleton, 
+    KanbanColumnSkeleton 
+};
